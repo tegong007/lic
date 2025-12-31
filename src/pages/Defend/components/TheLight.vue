4d23bcda59dcb78e9ab3dbb45fbed659cb1f49d5
jjjL <lijiajun@dascom.das.local>
2025-12-31 18:36:50 +0800

    fix

--- src/pages/Defend/components/TheLight.vue
+++ src/pages/Defend/components/TheLight.vue
@@ -1,31 +1,13 @@
 <template>
   <div class="bgItem text-1.1vw">
-    <template v-for="(light, index) in props.data" :key="index">
-      <div class="bgItem_tit">{{ light.lampName }}</div>
-      <section class="bg_jianbian mb-2vh ml-2vw flex">
-        <div v-if="props.act === 1" class="mr-3vw flex items-center">
-          <div class="ml-2vw pr-0.5vw">正反检测灯光:</div>
-          <a-button type="link" class="btn_search mr-1vw" @click="transferApi('/doc-machine/ctrl-lamp', light, true)">打开</a-button>
-          <a-button type="link" class="btn_search mr-1vw" @click="transferApi('/doc-machine/ctrl-lamp', light, false)">关闭</a-button>
-          <div class="ml-2vw pr-0.5vw">OCR灯光:</div>
-          <a-button type="link" class="btn_search mr-1vw" @click="transferApi('/doc-machine/ctrl-lamp', light, true)">打开</a-button>
-          <a-button type="link" class="btn_search mr-1vw" @click="transferApi('/doc-machine/ctrl-lamp', light, false)">关闭</a-button>
-        </div>
-        <div v-else-if="props.act === 2" class="mr-3vw flex items-center">
-          <div class="ml-2vw pr-0.5vw">激光定位灯光:</div>
-          <a-button type="link" class="btn_search mr-1vw" @click="transferApi('/doc-machine/ctrl-lamp', light, true)">打开</a-button>
-          <a-button type="link" class="btn_search mr-1vw" @click="transferApi('/doc-machine/ctrl-lamp', light, false)">关闭</a-button>
-        </div>
-        <div v-else-if="props.act === 3" class="mr-3vw flex items-center">
-          <div class="ml-2vw pr-0.5vw">喷墨定位灯光:</div>
-          <a-button type="link" class="btn_search mr-1vw" @click="transferApi('/doc-machine/ctrl-lamp', light, true)">打开</a-button>
-          <a-button type="link" class="btn_search mr-1vw" @click="transferApi('/doc-machine/ctrl-lamp', light, false)">关闭</a-button>
-          <div class="ml-2vw pr-0.5vw">成品检测灯光:</div>
-          <a-button type="link" class="btn_search mr-1vw" @click="transferApi('/doc-machine/ctrl-lamp', light, true)">打开</a-button>
-          <a-button type="link" class="btn_search mr-1vw" @click="transferApi('/doc-machine/ctrl-lamp', light, false)">关闭</a-button>
-        </div>
-      </section>
-    </template>
+    <div class="bgItem_tit">灯光</div>
+    <section class="bg_jianbian mb-2vh ml-2vw flex">
+      <div v-for="(light, index) in props.data" :key="index" class="mr-3vw flex items-center">
+        <div class="ml-2vw pr-0.5vw">{{ light.lampName }}:</div>
+        <a-button type="link" class="btn_search mr-1vw" @click="transferApi('/doc-machine/ctrl-lamp', light, true)">打开</a-button>
+        <a-button type="link" class="btn_search mr-1vw" @click="transferApi('/doc-machine/ctrl-lamp', light, false)">关闭</a-button>
+      </div>
+    </section>
   </div>
 </template>
 
@@ -42,7 +24,7 @@ async function transferApi(url: any, lightObj: any, trunOn: boolean) {
     useAppStore().setSpinning(true);
     const params = { transURI: url, paraIn: { jobUid: lightObj.jobUid, type: lightObj.type, isTurnOn: trunOn } };
     const data: any = await getApiTransfer(params);
-    if (data.rslts[0].code !== 0) throw data.rslts[0].msg || '未知错误';
+    if (data.code !== 0) throw data.msg || '未知错误';
     else notification.error({ message: '成功', description: '操作成功', class: 'notificationE-custom-class', placement: 'bottomRight' });
   } catch (error) {
     notification.error({ message: '错误', description: String(error), class: 'notificationE-custom-class', placement: 'bottomRight' });
